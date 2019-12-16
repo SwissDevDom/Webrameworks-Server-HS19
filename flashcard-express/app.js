d80b3625bb11c58ccb19db4f394ce0585579db57
Dominic Hein <dominic.hein@students.fhnw.ch>
2019-12-16 12:51:03 +0100

    AB25: Node Setup

---
+++ flashcard-express/app.js
@@ -0,0 +1,19 @@
+
+const config = require('dotenv-extended').load({
+    schema: '.env.schema',
+    errorOnMissing: true,
+    errorOnExtra: true
+});
+const log4js = require('log4js');
+const http = require('http');
+
+const logger = log4js.getLogger('App');
+logger.level = 'info';
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain'});
+    response.end('Hello World!\n')
+});
+
+server.listen(config.PORT);
+logger.info(`Server is running on port ${config.PORT}`);
