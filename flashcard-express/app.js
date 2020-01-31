c2e32556e70b374e2f4520e9859308bd4de80967
Dominic Hein <dominic.hein@students.fhnw.ch>
2020-01-31 12:06:26 +0100

    AB26: Integration MongoDB

--- flashcard-express/app.js
+++ flashcard-express/app.js
@@ -5,15 +5,28 @@ const config = require('dotenv-extended').load({
     errorOnExtra: true
 });
 const log4js = require('log4js');
-const http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const url = `mongodb://${ config.MONGO_HOST }/${ config.MONGO_DATABASE }`
+mongoose.connect(url, {useNewUrlParser: true});
 
 const logger = log4js.getLogger('App');
 logger.level = 'info';
 
-const express = require('express');
+const Questionnaire = require('./domain/questionnaire');
 const app = express();
 const port = config.PORT;
 
-app.get('/', (request, response) => response.send('Hello World!'));
+app.get('/', function(request, response) {
+    Questionnaire.find((err, questionnaires) => {
+        if (err) {
+            response.status(500).send(err)
+        }
+        response.send(questionnaires)
+    })
+});
 
-app.listen(port, () => console.log(`Server is running on port ${port}!`));
+app.listen(port, function () {
+    logger.info(`Server is running on port ${port}`)
+})
